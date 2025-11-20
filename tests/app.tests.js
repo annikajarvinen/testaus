f46ee5fc9dbe65f4298b287717a66e3e3b1f93cd
annikajarvinen <ag3931@student.jamk.fi>
2025-11-20 14:06:38 +0200

    test invorment

---
+++ tests/app.tests.js
@@ -0,0 +1,11 @@
+import { describe, test, expect, it } from 'vitest';
+const Laskin = require('../laskin/laskin');
+
+describe('Taskin luonnin testaus',
+
+  it('Should do stuff' , function () {
+  it('Should do other stuff'  
+  }
+  });
+
+
